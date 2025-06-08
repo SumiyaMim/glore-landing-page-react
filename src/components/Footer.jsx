1a1ccc593bc5930fe6e2f051c62c37e3f4d92159
SumiyaMim <sumiyaislam1093@gmail.com>
2025-06-09 00:06:28 +0600

    add policy components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-black py-14 px-4 md:px-9 lg:px-20'>
+      <div>
+        <div>
+            <img
+              src="/src/assets/images/logo.png"
+              alt="logo"
+              className="w-60"
+            />
+            <p>Our collection offers you a perfect blend of modern fashion and tradition.</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Policy.jsx
+++ src/components/Policy.jsx
@@ -9,17 +9,17 @@ const Policy = () => {
         <div className="flex flex-col items-center">
             <RiExchangeFundsFill className="text-5xl mb-5"/>
             <h3 className="font-semibold">Easy Exchange Policy</h3>
-            <h3>We Offer hassle free exchange policy</h3>
+            <p>We Offer hassle free exchange policy</p>
         </div>
         <div className="flex flex-col items-center">
             <MdVerified className="text-5xl mb-5"/>
             <h3 className="font-semibold">3 Days Return Policy</h3>
-            <h3>We provide 3 days free return policy</h3>
+            <p>We provide 3 days free return policy</p>
         </div>
         <div className="flex flex-col items-center">
             <PiHeadsetBold className="text-5xl mb-5"/>
             <h3 className="font-semibold">Best Customer Support</h3>
-            <h3>we provide 24/7 customer support</h3>
+            <p>we provide 24/7 customer support</p>
         </div>
       </div>
     </div>
