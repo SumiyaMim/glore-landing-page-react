2cc4952caebb038440c7a51241dc722a800845b3
SumiyaMim <sumiyaislam1093@gmail.com>
2025-06-09 00:07:18 +0600

    add banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,15 +7,15 @@ const Banner = () => {
                 <img src="/src/assets/images/logo.png" alt="logo" className="w-32 mb-5" />
             </div>
             <h1 className='text-4xl md:text-5xl lg:text-7xl font-semibold text-center lg:leading-20 mb-4 md:mb-5 lg:mb-8'>New <br /> <span className='text-[#C43882]'>Collection</span></h1>
-            <h3 className="text-sm md:text-lg lg:text-xl text-center mb-3 md:mb-0">
+            <p className="text-sm md:text-lg lg:text-xl text-center mb-3 md:mb-0">
             ✨ <span className="text-[#C43882]">GloreBD</span> - Step into a new era of fashion with us! ❤️
-            </h3>
-            <h3 className="text-lg lg:text-xl text-center hidden md:block">
+            </p>
+            <p className="text-lg lg:text-xl text-center hidden md:block">
             Our exclusive new collection is now available!
-            </h3>
-            <h3 className="text-lg lg:text-xl text-center hidden md:block mb-8 lg:mb-12">
+            </p>
+            <p className="text-lg lg:text-xl text-center hidden md:block mb-8 lg:mb-12">
             Discover your favorite fashion style and express your uniqueness. ❤️
-            </h3>
+            </p>
             <div className='flex justify-center items-center'>
                 <button className='bg-[#C43882] text-white rounded-xl px-4 md:px-9 py-1.5 md:py-3 text-base md:text-xl font-medium'>
                     Order Now
