bb66496efb197ee36953381e9f5fbfa3598b892e
SumiyaMim <sumiyaislam1093@gmail.com>
2025-06-09 18:09:44 +0600

    add single collection

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,5 +1,6 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import { FaStar } from "react-icons/fa6";
+import { FaMinus, FaPlus } from "react-icons/fa";
 
 const Collection = () => {
 
@@ -11,16 +12,16 @@ const Collection = () => {
 
   return (
     <div className="bg-[#ffe9ee] md:pt-32 md:pb-20 md:px-9 lg:px-20">
-        <div className="flex gap-12 items-start">
+        <div className="flex flex-col md:flex-row gap-12 items-start">
             <div>
                 <img
                     src={item.img}
                     alt={item.name}
-                    className="w-full max-w-md h-auto rounded cursor-pointer"
+                    className="w-full h-auto rounded cursor-pointer"
                 />
             </div>
-            <div>
-                <h1 className="text-3xl font-bold mb-5">{item.name}</h1>
+            <div className="w-full p-5 md:p-0">
+                <h1 className="text-3xl md:text-xl lg:text-3xl font-bold mb-5">{item.name}</h1>
                 <div className="flex gap-1 items-center mb-5">
                     <FaStar className="text-[#F59E0B] text-lg"/>
                     <FaStar className="text-[#F59E0B] text-lg"/>
@@ -28,8 +29,20 @@ const Collection = () => {
                     <FaStar className="text-[#F59E0B] text-lg"/>
                     <FaStar className="text-[#F59E0B] text-lg"/>
                 </div>
-                <p className="text-[#C43882] font-semibold text-3xl">৳ {item.price}</p>
-                
+                <h2 className="text-[#C43882] font-semibold text-3xl mb-5">৳ {item.price}</h2>
+                <p className="text-gray-500 mb-5">Category: {item.category}</p>
+                <div className="flex items-center gap-5 mb-5">
+                    <div className="p-2 border border-gray-200 rounded-md">
+                        <FaMinus/>
+                    </div>
+                    <h4>1</h4>
+                    <div className="p-2 border border-gray-200 rounded-md">
+                        <FaPlus/>
+                    </div>
+                </div>
+                <button className="bg-[#C43882] text-white cursor-pointer rounded py-2.5 w-full text-lg font-medium">
+                    Order Now
+                </button>
             </div>
         </div>
     </div>
